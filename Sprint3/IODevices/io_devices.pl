54e9ccca3353b68812d895af9b9db0354c491ee1
Sirius <159132908+sirius-22@users.noreply.github.com>
2025-08-07 12:30:00 +0200

    IoDevices Led fatto ✅
    con anche il modello di iodevices definitivo!
    Manca "solo" da testarlo sul raspeberry

---
+++ Sprint3/IODevices/io_devices.pl
@@ -0,0 +1,20 @@
+%====================================================================================
+% io_devices description   
+%====================================================================================
+mqttBroker("localhost", "1883", "unibo/qak/events").
+event( sonardata, distance(D) ).
+event( stopActions, stopActions(REASON) ).
+event( resumeActions, resumeActions(REASON) ).
+event( containerhere, containerhere(INFO) ).
+dispatch( ledon, ledon(M) ).
+dispatch( ledoff, ledoff(M) ).
+%====================================================================================
+context(ctx_raspdevice, "localhost",  "TCP", "8128").
+ qactor( mind, ctx_raspdevice, "it.unibo.mind.Mind").
+ static(mind).
+  qactor( sonardevice, ctx_raspdevice, "it.unibo.sonardevice.Sonardevice").
+ static(sonardevice).
+  qactor( leddevice, ctx_raspdevice, "it.unibo.leddevice.Leddevice").
+ static(leddevice).
+  qactor( sonarsimul, ctx_raspdevice, "it.unibo.sonarsimul.Sonarsimul").
+ static(sonarsimul).
